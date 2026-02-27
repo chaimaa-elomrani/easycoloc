37da1ec8fa8ee4d2ac1d747fa5518b6641052c5e
sheima <chaimaelomrani6@gmail.com>
2026-02-27 15:32:11 +0000

    created the needed tables to start the backend

---
+++ database/migrations/2026_02_27_150050_create_categories_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('colocation_id')->constrained()->onDelete('cascade');
+            $table->string('name')->unique(); 
+            $table->timestamps();
+
+            $table->unique(['colocation_id', 'name']); 
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
