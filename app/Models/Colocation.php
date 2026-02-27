26657a7c2e8b65f7daa6e619ee0899bda6bcb2de
sheima <chaimaelomrani6@gmail.com>
2026-02-27 15:47:11 +0000

    added the relations needed in coloctaionmodel

--- app/Models/Colocation.php
+++ app/Models/Colocation.php
@@ -7,4 +7,35 @@ use Illuminate\Database\Eloquent\Model;
 class Colocation extends Model
 {
     //
-}
+
+
+
+    public function owner(){
+        return $this->belongsTo(User::class , 'owner_id');
+    }
+
+    public function members(){
+        return $this->belongsToMany(User::class , 'memberships')->withPivot('role', 'joined_at' , 'left_at');
+    }
+
+    public function activeMembers(){
+    return $this->members()->whereNull('memberships.left_at');
+    }
+
+    public function expenses(){
+        return $this->hasMany(Expense::class);
+    }
+
+    public function categories(){
+        return $this->hasMany(Category::class);
+    }
+
+    public function settlements(){
+        return $this->hasMany(Settlement::class);
+    }
+
+    public function invitations(){
+        return $this->hasMany(Invitation::class);
+    }
+
+    }
